9200d80112c2b6cb3268dcf2b42ce8d1325fce6c
MeerKateAnimation <MeerKateAnimation@users.noreply.github.com>
2023-07-07 15:05:59 -0500

    Update createGearHiarchy.py
    -Fixed problem if selected gears have rotation on them they would rotate the wrong way
    -Added attr to offset control to override the rotation direction
    -a few other small fixes to organize and such

--- createGearHiarchy.py
+++ createGearHiarchy.py
@@ -7,10 +7,13 @@ should get rotation and scale(?) of gears too
 should lock pos neg and maybe even hide it?
 if UI popups are an option then make the temp gears number pop up as a prompt
 make loops consistant (x+1 vs x)
-can I extrude the tempGear to give it cogs?
 save each main group and parent them all into a single group
 currently breaks if you try and do it mutltiple times in same scene
 add attribute to change how far out from the gear the offset control is
+add attribute to change visible orientation??? 
+set control to go to first gear (or be moved by animator? but default is by first gear))
+if gears are zeroed out the offset control will not be in the right place. Gear rotation still works as intended
+maybe add an attribute to offset 'direction override' or something like that to reverse the direction if needed may be easier then rotating the offset by 180 degrees
 
 '''
 
@@ -49,7 +52,8 @@ def createCon(): # function that creates the control (replace later with arrow c
     # creates the control
     print('Creating gear control')
     con = cmds.circle(n = 'gear_CON', d = 3, s = 8, nr = (1, 0, 0), cx = 1, r = 1.5, ch = False)
-    
+    grps = [con]
+    print(grps)
     # add attributes to control
     cmds.select(con, r = True)
     cmds.addAttr(ln = 'rotMult', at = 'float', dv = 1, h = False, k = True)
@@ -64,7 +68,8 @@ saves gear positions and creates gears if necessary
 '''
 gearGeo = cmds.ls(sl = True)
 gearPos = {}
-grps = []
+gearRot = {}
+#grps = []
 
 if gearGeo == []:
     print('Nothing is selected - creating temporary meshes in place of gears')
@@ -76,28 +81,30 @@ if gearGeo == []:
         gearGeo.append(gear)
         gearPos[gearGeo[i]] = [0.0, 0.0, tempGearOffset]
         tempGearOffset = tempGearOffset + tempGearOffsetDistance
-        print(gearGeo)
-        print(gear)
 
 else:
     gearAmount = len(gearGeo)
     print('Saving positions of selected gears')
     createdGear = False
     for j in range(gearAmount):
-        pos = cmds.xform(gearGeo[j], q = True, t = True, ws= True)
+        pos = cmds.xform(gearGeo[j], q = True, t = True, ws = True)
+        rot = cmds.xform(gearGeo[j], q = True, ro = True, ws = True)
         gearPos[gearGeo[j]] = pos
+        gearRot[gearGeo[j]] = rot
    
 ''' 
 creating the rig 
 '''
 # create control for gears
 rotInput = createCon()
+print(grps)
 
 # main loop for creating the gear rig
+print(gearGeo)
 for x in range(1, gearAmount + 1): 
     print('Setting up rig for gear ' + str(x))
     # center gear and create hiarchy
-    cmds.xform(gearGeo[x-1], t = (0.0, 0.0, 0.0), ws = True)
+    cmds.xform(gearGeo[x-1], t = (0.0, 0.0, 0.0), ro = (0,0,0), ws = True, a = True)
     cmds.select(gearGeo[x-1])
     geoGrp = cmds.group(n = 'geoGrp{}'.format(x))
     rotGrp = cmds.group(n = 'rotGrp{}'.format(x))
@@ -107,6 +114,7 @@ for x in range(1, gearAmount + 1):
     cmds.parent(rotGrp, gearOffset)
     
     cmds.addAttr(gearOffset, at = 'long', longName = 'gearCogs', defaultValue = 10, minValue = 2, h = False, k = True)
+    cmds.addAttr(gearOffset, at = 'bool', longName = 'directionOverride', defaultValue = False, h = False, k = True)
     
     # create node to calculate rotation ratio
     cogConversion = cmds.createNode('multiplyDivide', n = ("cogConversion{}".format(x)))
@@ -118,6 +126,12 @@ for x in range(1, gearAmount + 1):
     # create node to invert direction
     rotInvert = cmds.createNode('multiplyDivide', n = ('rotInvert{}'.format(x)))
     cmds.setAttr('{}.input2X'.format(rotInvert), -1)
+    
+    #create override direction condition node
+    override = cmds.createNode('condition', n = ('invertOverride{}'.format(x)))
+    cmds.setAttr('invertOverride{}.secondTerm'.format(x), 1)
+    cmds.setAttr('invertOverride{}.colorIfTrueR'.format(x), 1)
+    cmds.setAttr('invertOverride{}.colorIfFalseR'.format(x), -1)
 
     # connect attributes
     cmds.connectAttr('{}.gearCogs'.format(gearOffset), '{}.input1X'.format(cogConversion))
@@ -125,17 +139,24 @@ for x in range(1, gearAmount + 1):
     cmds.connectAttr('{}.outputX'.format(rotCalc), '{}.input1X'.format(rotInvert))
     cmds.connectAttr('{}.outputX'.format(rotInput), '{}.rotateX'.format(rotGrp))
     cmds.connectAttr('{}.outputX'.format(cogConversion), '{}.input2X'.format(rotCalc))
+    cmds.connectAttr('{}.outColorR'.format(override), '{}.input2X'.format(rotInvert))
+    
 
     if x != 1:
         # connects nodes from the last gear to the current gear's nodes
         cmds.connectAttr('{}.gearCogs'.format(gearOffset), '{}.input2X'.format(lastCogConversion))
+        cmds.connectAttr('{}.directionOverride'.format(gearOffset), '{}.firstTerm'.format(lastOverride))
 
     # moves the gear back to assigned offset position
-    cmds.xform(gearOffset, t = gearPos[gearGeo[x-1]], ws = True)
+    cmds.xform(gearOffset, t = gearPos[gearGeo[x-1]], ro = gearRot[gearGeo[x-1]], ws = True)
     
     # relabel variables
     lastCogConversion = cogConversion
-    lastRotGrp = rotGrp
+    lastRotGrp = rotGrp #i don't think this one is needed anymore
     rotInput = rotInvert
+    lastOverride = override
+    
+#organizing the end result
+cmds.group(grps, n = 'gear rig')
     
 endMessage()
