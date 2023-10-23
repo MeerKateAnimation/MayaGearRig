58cb33445ec313bb990664ed1a7d463d24285f78
MeerKateAnimation <51034011+MeerKateAnimation@users.noreply.github.com>
2023-10-23 14:21:58 -0500

    Update createGearRig.py
    Expand how to use section

--- createGearRig.py
+++ createGearRig.py
@@ -15,7 +15,8 @@ attr to change default gear cog number?
 Arrange your gears' position and rotation
 Select your gears in order
 Run program
-
+Select offset controls and enter the number of cogs for each gear
+Rotate main control and watch all the other gears rotate too!
 '''
 
 ''' ----------------------------------------------------------
